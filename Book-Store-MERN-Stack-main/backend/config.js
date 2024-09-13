2c2573b8bb7b94a034923cc828a3bb79fda98a4a
SudaisAkbar <msudaisakbar434@gmail.com>
2024-09-13 14:05:41 +0500

    Initial Commit

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://msudaisakbar434:P5rAfn%23DWE_6CvJ@books-store-mern.klic7.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN'
